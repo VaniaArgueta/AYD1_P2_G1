e30a38a1817fea06a6f675601e1c91471f99d21e
200413657 <reningesis@gmail.com>
2023-03-11 00:49:53 -0600

    modificadiones conexion,index

--- backend/conexion.js
+++ backend/conexion.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 var config = {
-  host: "",
-  user: "",
-  password: "",
-  database: "",
-  port: ,
+  host: "db4free.net",
+  user: "rooty4",
+  password: "admin123#",
+  database: "ayd1_p2",
+  port:3306 ,
 };
 const conn = new mysql.createConnection(config);
 
