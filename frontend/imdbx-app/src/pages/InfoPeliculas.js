29de437bc97d994137e7deb438dfd2a5bff69fea
200413657 <reningesis@gmail.com>
2023-03-10 22:33:32 -0600

    se movió dos querys de infopeliculas de index.js

--- frontend/imdbx-app/src/pages/InfoPeliculas.js
+++ frontend/imdbx-app/src/pages/InfoPeliculas.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-export const InfoPeliculas = () => {
+export const InfoPeliculas = (prop) => {
+  
+  const url = "http://localhost:3000/infoPeliculas";
+  const [datosAPI,setDatosAPI] = useState([]);
+
+  React.useEffect(
+    
+  );
   return (
     <>
       <p className="datosPrincipales">INFO PELÍCULAS</p>
