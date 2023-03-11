5be146ebfdd35009c0420276fc124aa72d259079
lecx <lecx0110@gmail.com>
2023-03-11 11:11:08 -0600

    feature/catalogoPelicula_201213505
    se agrega paginado y catalogo principal de peliculas

--- frontend/imdbx-app/src/pages/CatalogoPeliculas.js
+++ frontend/imdbx-app/src/pages/CatalogoPeliculas.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
+import '../Catalogo.css';
+import Pagination from './Pagination';
+import MovieCard from './MovieCard';
 
-export const CatalogoPeliculas = () => {
-  return (
-    <>
-      <p className="datosPrincipales">CATALOGO PELÍCULAS</p>      
-    </>
-  )
+class CatalogoPeliculas extends Component {
+  state = { allMovies: [], currentMovies: [], currentPage: null, totalPages: null }
+
+  componentDidMount() {
+    const { data: allMovies = [] } = this.props.movies || [];
+    this.setState({ allMovies });
+  }
+
+  onPageChanged = data => {
+    const { allMovies } = this.state;
+    const { currentPage, totalPages, pageLimit } = data;
+    const offset = (currentPage - 1) * pageLimit;
+    const currentMovies = allMovies.slice(offset, offset + pageLimit);
+
+    this.setState({ currentPage, currentMovies, totalPages });
+  }
+
+  render() {
+    const { allMovies, currentMovies, currentPage, totalPages } = this.state;
+    const totalMovies = allMovies.length;
+
+    if (totalMovies === 0) return null;
+
+    const headerClass = ['text-light py-2 pr-4 m-0', currentPage ? 'border-gray border-right' : ''].join(' ').trim();
+
+    return (
+      <div className="containerC">
+        <div className="rowS">
+          <div className="rowHe">
+            <div className="d-flex flex-row align-items-center">
+              <h2 className={headerClass}>
+                <strong className="text-secondary">{totalMovies}</strong> Peliculas
+              </h2>
+              {currentPage && (
+                <span className="current-page d-inline-block h-100 pl-4 text-secondary">
+                  página <span className="font-weight-bold">{currentPage}</span> / <span className="font-weight-bold">{totalPages}</span>
+                </span>
+              )}
+            </div>
+            <div className="align-items-right">
+              <Pagination totalRecords={totalMovies} pageLimit={10} pageNeighbours={1} onPageChanged={this.onPageChanged} />
+            </div>
+          </div>
+          <div className='rowBo'>
+            {currentMovies.map(movie => <MovieCard key={movie.idPelicula} movie={movie} />)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default CatalogoPeliculas;
---
+++ frontend/imdbx-app/src/pages/MovieCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieCard = props => {
+    const {
+        nombre_actor = 'unknown', año = 0, nombre = '', ilustracion = ''
+    } = props.movie || {};
+
+    return (
+        <div className="col-sm-6 col-md-8 movie-card">
+            <div className="movie-card-container border-gray rounded border mx-2 d-flex flex-row align-items-center p-0 bg-light">
+                <div className="h-100 position-relative border-gray border-right px-2 bg-white rounded-left">
+                    <img src={ilustracion} alt="ProfilePic" className='img-thumbnail img-fluid rounded' style={{ maxWidth: "100px", maxHeight: "88px", padding: "2px" }} />
+                </div>
+                <div className="col-md-7" style={{ height: '90%' }}>
+                    <label className='movie-lb-titulo'>Titulo: </label>
+                    <span className="movie-nombre text-dark font-weight-bold">{nombre}</span>
+                    <label className='movie-lb-titulo'>Año: </label>
+                    <span className="movie-año text-dark d-block font-weight-bold">{año}</span>
+                    <label className='movie-lb-titulo'>Actor: </label>
+                    <span className="movie-nombre-actor d-block font-weight-bold">{nombre_actor}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        año: PropTypes.number.isRequired,
+        nombre: PropTypes.string.isRequired
+    }).isRequired
+};
+
+export default MovieCard;
